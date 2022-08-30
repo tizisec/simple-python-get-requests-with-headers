7cc61c38b418ba2db07bc4a1e9098d46d6cca576
alireza.tizghadam <50079044+tizisec@users.noreply.github.com>
2022-08-30 12:11:34 +0430

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 import requests as req
 
-		headers = {'Cookie': ''}
-		resp = req.get(url="example.com",headers=headers)
-			print("status code is: ",resp.status_code)
+headers = {'Cookie': ''}
+resp = req.get(url="example.com",headers=headers)
+print("status code is: ",resp.status_code)
